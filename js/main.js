4be1ec6c33f333b6109e5b54f16bc7708e1bc9cc
Emanuel Pilz <emonadeo@gmail.com>
2023-05-20 23:51:26 +0200

    Moved april fools style to secret button ;)

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 import routes from './routes.js';
 
 export const store = Vue.reactive({
-    dark: JSON.parse(localStorage.getItem('dark')) || false,
-    toggleDark() {
-        this.dark = !this.dark;
-        localStorage.setItem('dark', JSON.stringify(this.dark));
-    },
+	dark: JSON.parse(localStorage.getItem('dark')) || false,
+	shitty: JSON.parse(localStorage.getItem('shitty')) || false,
+	toggleDark() {
+		this.dark = !this.dark;
+		localStorage.setItem('dark', JSON.stringify(this.dark));
+	},
+	toggleShitty() {
+		this.shitty = !this.shitty;
+		localStorage.setItem('shitty', JSON.stringify(this.shitty));
+	},
 });
 
 const app = Vue.createApp({
-    data: () => ({ store }),
+	data: () => ({ store }),
 });
 const router = VueRouter.createRouter({
-    history: VueRouter.createWebHashHistory(),
-    routes,
+	history: VueRouter.createWebHashHistory(),
+	routes,
 });
 
 app.use(router);
